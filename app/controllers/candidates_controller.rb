9650e0329dfe9770e52c0d04fce8481c21b08989
s8852s <s8852s@hotmail.com.tw>
2020-11-07 10:43:09 +0800

    新增候選人 候選人展示頁面

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,34 @@
+class CandidatesController < ApplicationController
+
+	def index
+		@candidates = Candidate.all
+	end
+
+	def show
+		@candidate = Candidate.find_by(id: params[:id])
+	end
+
+
+  def new
+		@candidate = Candidate.new
+	end
+	
+	def create
+		@candidate = Candidate.new(candidate_params)
+
+		if @candidate.save
+			flash[:notice] = "Candidate created!"
+			redirect_to "/candidates"
+		else
+			#NG
+			render :new
+
+		end
+	end
+		
+		private
+		def candidate_params
+			params.require(:candidate).permit(:name, :party, :age, :politics)
+		end
+
+end
